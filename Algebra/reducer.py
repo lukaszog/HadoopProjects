8ec6da1b7e596434a48854d35a99527e34747f45
lukaszog <lukasz.ogan@gmail.com>
2017-03-27 10:32:43 +0200

    przeciecie

--- Algebra/reducer.py
+++ Algebra/reducer.py
@@ -1,14 +1,22 @@
 import sys
 
+# przeciecie zbiorow
 
 def main():
+    comp_key = -1
+    comp_value = -1
     for incoming in sys.stdin:
         line = incoming.strip()
         elements = line.split('\t')
 
-        key = elements[1]
-        value = elements[0]
+        key = elements[0]
+        value = elements[1]
 
-        print str(key) + str(value)
+        if key == comp_key and not value == comp_value:
+            print str(key) + str(value)
+
+        comp_key = key
+        comp_value = value
+
 
 main()
